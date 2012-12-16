7f45e008f50985726b66a048c26ac4161b46019d
Daniel <daxx4182@gmail.com>
2012-12-17 04:41:52 +0800

    Some tweaks by Noah
    Change-Id: I6048cc79e9f0c283bbcc5d8cb47425ba2dc03585

--- power/power_tuna.c
+++ power/power_tuna.c
@@ -130,7 +130,8 @@ static void tuna_power_set_interactive(struct power_module *module, int on)
      * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
      * cpufreq policy.
      */
-    sysfs_write(SCREENOFFMAXFREQ_PATH,screen_off_max_freq);
+    sysfs_write(("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
+                on ? "1200000" : "700000");
 }
 
 static void tuna_power_hint(struct power_module *module, power_hint_t hint,
