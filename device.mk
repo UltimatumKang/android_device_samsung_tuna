ceac826a410db1a9ed252c2c6ba6d6a94e267cf9
Daniel <daxx4182@gmail.com>
2013-03-26 16:39:25 +0800

    @MBQSniper

--- device.mk
+++ device.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
 	device/samsung/tuna/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
 PRODUCT_PROPERTY_OVERRIDES := \
-	wifi.interface=wlan0
+	wifi.interface=wlan0 \
+        wifi.supplicant_scan_interval=300
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
